5d5297e445cc4e9d3e30eb61f104dbc08fff449a
Hypermona <mohans8050@gmail.com>
2021-06-22 14:18:56 +0530

    roll back

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 #Stopping existing node servers
-
-# isExistApp = `pgrep node`
-# if [[ -n  $isExistApp ]]; then
-#     echo "Stopping any existing node servers"
-#     pkill node        
-# fi
+echo "Stopping any existing node servers"
+pkill node
