f4abd490c30336a6b4343bb9b35ba9200da2fbb8
Hypermona <mohans8050@gmail.com>
2021-06-22 14:11:04 +0530

    test 1

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #Stopping existing node servers
 
-isExistApp = `pgrep node`
-if [[ -n  $isExistApp ]]; then
-    echo "Stopping any existing node servers"
-    pkill node        
-fi
+# isExistApp = `pgrep node`
+# if [[ -n  $isExistApp ]]; then
+#     echo "Stopping any existing node servers"
+#     pkill node        
+# fi
